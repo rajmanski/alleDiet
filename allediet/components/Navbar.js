c36b7326891b0dec50125ed5bbd706ba5530ded1
Piotr Rajmanski <prajmanski10@gmail.com>
2023-01-26 12:02:09 +0100

    Link to home page and to articles page added on navbar

--- allediet/components/Navbar.js
+++ allediet/components/Navbar.js
@@ -1,22 +1,25 @@
 import Image from "next/image"
+import Link from "next/link"
 
 export const Navbar = () => {
     return (
         <>
             <nav className="py-3 fixed top-0 left-0 right-0 bottom-auto shadow-sm font-lato z-20 bg-white opacity-95">
                 <div className="flex flex-row items-center mx-auto justify-around text-center max-w-7xl">
+                <Link href="/">
                     <div id="logo" className="flex flex-row gap-2 items-center ">
                         <Image width="48" height="48" src="/eggplant2.png" alt="eggplant" />
                         <p className="text-5xl hover:scale-100 font-moja">alle diet</p>
-                    </div>
+                    </div></Link>
                     <div id="right-section" className="flex flex-row gap-5 items-center font-light">
                         <p className="hover:text-violet text-lg">O mnie</p>
-                        <p className="hover:text-violet text-lg">Artykuły</p>
+                        <Link href="/articles"><p className="hover:text-violet text-lg">Artykuły</p></Link>
                         <p className="hover:text-violet text-lg">Cennik</p>
                         <p className="hover:text-violet text-lg">Kontakt</p>
                         <p className="hover:text-violet hover:text-xl text-lg border p-2 border-solid border-violet rounded-2xl text-violet">Umów wizytę</p>
                     </div>
                 </div>
+                
             </nav>
         </>
     )
--- allediet/pages/articles.js
+++ allediet/pages/articles.js
@@ -23,6 +23,34 @@ export default function Articles() {
                             <button className="px-6 py-3 bg-violet text-white rounded-2xl text-md hover:bg-hoverViolet">Czytaj więcej</button>
                         </div>
                     </div>
+                    <div id="article" className="my-10 flex flex-row gap-10 font-thin">
+                        <div id="imageSection" className="w-1/4 h-auto">
+                            <Image width="600" height="600" src="/mieta.jpg" alt="mieta" className="h-full"/>
+                        </div>
+                        <div id="articleInfo" className="w-3/4 flex flex-col items-start gap-4">
+                            <h2 className="text-violet text-3xl hover:underline cursor-pointer">Właściwości lecznicze mięty</h2>
+                            <div id="dateAndAuthorWrapper" className="flex flex-row gap-6">
+                                <p>22 listopada 2022</p>
+                                <span className="font-normal">autor: Aleksandra Dublas</span>
+                            </div>
+                            <p>Mięta pieprzowa, znana i popularna szczególnie latem, nie tylko zdobi nasze szklanki z wodą i cytryną dając nutę orzeźwienia w niesamowitym upale, ale także ma wiele właściwości zdrowotnych. Liście tego przepięknie pachnącego zioła mogą pływać w szkle praktycznie o każdej porze roku.</p>
+                            <button className="px-6 py-3 bg-violet text-white rounded-2xl text-md hover:bg-hoverViolet">Czytaj więcej</button>
+                        </div>
+                    </div>
+                    <div id="article" className="my-10 flex flex-row gap-10 font-thin">
+                        <div id="imageSection" className="w-1/4 h-auto">
+                            <Image width="600" height="600" src="/mieta.jpg" alt="mieta" className="h-full"/>
+                        </div>
+                        <div id="articleInfo" className="w-3/4 flex flex-col items-start gap-4">
+                            <h2 className="text-violet text-3xl hover:underline cursor-pointer">Właściwości lecznicze mięty</h2>
+                            <div id="dateAndAuthorWrapper" className="flex flex-row gap-6">
+                                <p>22 listopada 2022</p>
+                                <span className="font-normal">autor: Aleksandra Dublas</span>
+                            </div>
+                            <p>Mięta pieprzowa, znana i popularna szczególnie latem, nie tylko zdobi nasze szklanki z wodą i cytryną dając nutę orzeźwienia w niesamowitym upale, ale także ma wiele właściwości zdrowotnych. Liście tego przepięknie pachnącego zioła mogą pływać w szkle praktycznie o każdej porze roku.</p>
+                            <button className="px-6 py-3 bg-violet text-white rounded-2xl text-md hover:bg-hoverViolet">Czytaj więcej</button>
+                        </div>
+                    </div>
                 </div>
             </section>
         </>

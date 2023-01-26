51793ec2657654df382fede20c73d3df51223910
Piotr Rajmanski <prajmanski10@gmail.com>
2023-01-26 12:16:32 +0100

    responsivnes added to /artciles

--- allediet/pages/articles.js
+++ allediet/pages/articles.js
@@ -9,11 +9,11 @@ export default function Articles() {
                 <div className="w-5/6">
                     <h1 className="text-6xl text-center text-violet mb-8">Baza wiedzy</h1>
                     <hr />
-                    <div id="article" className="my-10 flex flex-row gap-10 font-thin">
-                        <div id="imageSection" className="w-1/4 h-auto">
+                    <div id="article" className="my-10 flex flex-row gap-10 font-thin lg:flex-wrap lg:justify-center md:gap-5">
+                        <div id="imageSection" className="w-2/6 h-auto lg:w-4/6">
                             <Image width="600" height="600" src="/mieta.jpg" alt="mieta" className="h-full"/>
                         </div>
-                        <div id="articleInfo" className="w-3/4 flex flex-col items-start gap-4">
+                        <div id="articleInfo" className="w-4/6 flex flex-col items-start gap-4">
                             <h2 className="text-violet text-3xl hover:underline cursor-pointer">Właściwości lecznicze mięty</h2>
                             <div id="dateAndAuthorWrapper" className="flex flex-row gap-6">
                                 <p>22 listopada 2022</p>
@@ -23,11 +23,11 @@ export default function Articles() {
                             <button className="px-6 py-3 bg-violet text-white rounded-2xl text-md hover:bg-hoverViolet">Czytaj więcej</button>
                         </div>
                     </div>
-                    <div id="article" className="my-10 flex flex-row gap-10 font-thin">
-                        <div id="imageSection" className="w-1/4 h-auto">
+                    <div id="article" className="my-10 flex flex-row gap-10 font-thin lg:flex-wrap lg:justify-center md:gap-5">
+                        <div id="imageSection" className="w-2/6 h-auto lg:w-4/6">
                             <Image width="600" height="600" src="/mieta.jpg" alt="mieta" className="h-full"/>
                         </div>
-                        <div id="articleInfo" className="w-3/4 flex flex-col items-start gap-4">
+                        <div id="articleInfo" className="w-4/6 flex flex-col items-start gap-4">
                             <h2 className="text-violet text-3xl hover:underline cursor-pointer">Właściwości lecznicze mięty</h2>
                             <div id="dateAndAuthorWrapper" className="flex flex-row gap-6">
                                 <p>22 listopada 2022</p>
@@ -37,11 +37,11 @@ export default function Articles() {
                             <button className="px-6 py-3 bg-violet text-white rounded-2xl text-md hover:bg-hoverViolet">Czytaj więcej</button>
                         </div>
                     </div>
-                    <div id="article" className="my-10 flex flex-row gap-10 font-thin">
-                        <div id="imageSection" className="w-1/4 h-auto">
+                    <div id="article" className="my-10 flex flex-row gap-10 font-thin lg:flex-wrap lg:justify-center md:gap-5">
+                        <div id="imageSection" className="w-2/6 h-auto lg:w-4/6">
                             <Image width="600" height="600" src="/mieta.jpg" alt="mieta" className="h-full"/>
                         </div>
-                        <div id="articleInfo" className="w-3/4 flex flex-col items-start gap-4">
+                        <div id="articleInfo" className="w-4/6 flex flex-col items-start gap-4">
                             <h2 className="text-violet text-3xl hover:underline cursor-pointer">Właściwości lecznicze mięty</h2>
                             <div id="dateAndAuthorWrapper" className="flex flex-row gap-6">
                                 <p>22 listopada 2022</p>

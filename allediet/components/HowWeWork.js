1a20a22b176038619655d1118947bea0e86f4ec4
Piotr Rajmanski <prajmanski10@gmail.com>
2023-01-26 10:22:55 +0100

    Icons added to howwework section

--- allediet/components/HowWeWork.js
+++ allediet/components/HowWeWork.js
@@ -1,19 +1,41 @@
+import Image from "next/image";
+
 export const HowWeWork = () => {
   return (
-    <section className="flex flex-col mx-auto max-w-7xl items-center justify-center font-thin mb-40">
+    <section className="flex flex-col mx-auto max-w-7xl items-center justify-center font-thin mb-40 font-lato">
       <div className="flex flex-col mx-auto">
-        <h1 className="text-center text-4xl pb-10 leading-normal">Jak działamy?</h1>
+        <h1 className="text-center text-4xl pb-10 leading-normal">
+          Jak działamy?
+        </h1>
         <div id="boxWrapper" className="flex flex-row gap-10">
-          <div id="box" className="flex flex-col justify-around gap-10">
-            <h3 className="text-3xl text-violet">1. Umawiasz się na wizytę</h3>
+          <div id="box" className="flex flex-col justify-between gap-8">
+            <div className="flex-col gap-4 flex">
+              <Image
+                width="48"
+                height="48"
+                src="/calendar.png"
+                alt="eggplant"
+              />
+              <h3 className="text-3xl text-violet">
+                Umawiasz się na wizytę
+              </h3>
+            </div>
             <p>
               Przy umówieniu na wizytę wybierasz dogodny dal siebie termin oraz
               zaznaczasz z jaką dolegliwością sie do nas zgłaszasz, w celu
               rzetelnego pyrzgotowania się do wizyty.
             </p>
           </div>
-          <div id="box" className="flex flex-col justify-around  gap-10">
-            <h3 className="text-3xl text-violet">2. Odbywasz wizytę</h3>
+          <div id="box" className="flex flex-col justify-between  gap-8">
+            <div className="flex-col flex gap-4">
+              <Image
+                width="48"
+                height="48"
+                src="/stethoscope.png"
+                alt="eggplant"
+              />
+              <h3 className="text-3xl text-violet">Odbywasz wizytę</h3>
+            </div>
             <p>
               Wizyty odbywają się zarówno zdalnie jak i na miejscu. Na wizycie
               pacjent może w pełni poinformować dietetyka o swoich
@@ -21,8 +43,18 @@ export const HowWeWork = () => {
               diagonostyczne badania krwi w celu diagnostycznym.
             </p>
           </div>
-          <div id="box" className="flex flex-col justify-around  gap-10">
-            <h3 className="text-3xl text-violet">3. Pojawiasz się z wynikami badań u dietetyka</h3>
+          <div id="box" className="flex flex-col justify-between  gap-8">
+            <div className="flex-col flex gap-4">
+              <Image
+                width="48"
+                height="48"
+                src="/doctorsbag.png"
+                alt="eggplant"
+              />
+              <h3 className="text-3xl text-violet">
+                Pojawiasz się z wynikami badań u dietetyka
+              </h3>
+            </div>
             <p>
               Ostatnim etapem diagnostycznym jest weryfikacaj badań lekarskich,
               które powinny powiedzieć dietetykowi, jakie są przyczyny złego

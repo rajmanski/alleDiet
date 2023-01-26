de72fc481fa67def241f720a72bd81b02d7d8dc4
Piotr Rajmanski <prajmanski10@gmail.com>
2023-01-26 09:36:31 +0100

    margin bottom added on whatwedo section

--- allediet/components/WhatWeDo.js
+++ allediet/components/WhatWeDo.js
@@ -23,7 +23,7 @@ export const WhatWeDo = () => {
   }, [])
 
     return (
-        <div className="bg-gray-200  text-center mx-auto p-12 h-60">
+        <div className="bg-gray-200  text-center mx-auto p-12 h-60 mb-20">
             <section className="max-w-7xl font-lato mx-auto flex flex-col gap-10">
                 <h2 className="text-4xl leading-normal font-thin ">Jakimi dolegliwościami się zajmujemy?</h2>
                 <p className="text-6xl text-violet"><span className='font-sans' ref={el}></span></p>

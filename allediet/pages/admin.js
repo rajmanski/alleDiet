64d8e86857b8050706ee38c1559393ada8a414dc
Piotr Rajmanski <prajmanski10@gmail.com>
2023-01-30 14:44:10 +0100

    New post form added

--- allediet/pages/admin.js
+++ allediet/pages/admin.js
@@ -8,6 +8,7 @@ import { useEffect, useState } from "react";
 export default function Admin()  {
     
     const [email, setEmail] = useState(null);
+    const [showPost, setShowPost] = useState(false);
     const router = useRouter();
     const user = useAuth();
     // console.log(user.user?.email);
@@ -19,11 +20,26 @@ export default function Admin()  {
     return (
         <>
             <Navbar/>
-            <div className="mt-40 mx-auto text-center">Strona admina, tutaj można dodać nowy post</div>
+            <div className="mt-40 mx-auto text-center"></div>
             {auth.currentUser?.email === "prajmanski10@gmail.com" && (
-                <div className="mt-40 mx-auto text-center">
-                    <p>{auth.currentUser.email}</p>
-                    <button>Dodaj nowy post</button>
+                <div className="mt-20 mx-auto text-center max-w-4xl items-center justify-center">
+
+                    <button onClick={() => setShowPost(!showPost)} className="text-4xl text-violet p-3 m-4" >Dodaj nowy post</button>
+                    <div id="postSection">
+                        {showPost && (
+                            <div className="flex flex-col font-lato gap-5">
+                                <p className="text-violet">Tytuł postu: </p>
+                                <input type="text" />
+                                <p className="text-violet">Opis postu: </p>
+                                <input type="text" />
+                                <p className="text-violet">Body postu: </p>
+                                <textarea/>
+                                <button className="bg-violet p-2">Załącz zdjęcie</button>
+                                <button className="bg-violet p-2">Dodaj post</button>
+                            </div>
+                            
+                        )}
+                    </div>
                 </div>
             )}
         </>
